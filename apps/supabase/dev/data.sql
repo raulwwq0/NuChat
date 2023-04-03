c7bd8769825272114c82dbf313d60e626526e56b
Raúl Aguilar <ag21raul@gmail.com>
2023-04-03 20:26:33 +0200

    feat: create tables at start

--- apps/supabase/dev/data.sql
+++ apps/supabase/dev/data.sql
@@ -1,48 +1,309 @@
-create table profiles (
-  id uuid references auth.users not null,
-  updated_at timestamp with time zone,
-  username text unique,
-  avatar_url text,
-  website text,
+-- create table profiles (
+--   id uuid references auth.users not null,
+--   updated_at timestamp with time zone,
+--   username text unique,
+--   avatar_url text,
 
-  primary key (id),
-  unique(username),
-  constraint username_length check (char_length(username) >= 3)
+--   primary key (id),
+--   unique(username),
+--   constraint username_length check (char_length(username) >= 3)
+-- );
+
+-- alter table profiles enable row level security;
+
+-- create policy "Public profiles are viewable by the owner."
+--   on profiles for select
+--   using ( auth.uid() = id );
+
+-- create policy "Users can insert their own profile."
+--   on profiles for insert
+--   with check ( auth.uid() = id );
+
+-- create policy "Users can update own profile."
+--   on profiles for update
+--   using ( auth.uid() = id );
+
+-- -- Set up Realtime
+-- begin;
+--   drop publication if exists supabase_realtime;
+--   create publication supabase_realtime;
+-- commit;
+-- alter publication supabase_realtime add table profiles;
+
+-- -- Set up Storage
+-- insert into storage.buckets (id, name)
+-- values ('avatars', 'avatars');
+
+-- create policy "Avatar images are publicly accessible."
+--   on storage.objects for select
+--   using ( bucket_id = 'avatars' );
+
+-- create policy "Anyone can upload an avatar."
+--   on storage.objects for insert
+--   with check ( bucket_id = 'avatars' );
+
+-- create policy "Anyone can update an avatar."
+--   on storage.objects for update
+--   with check ( bucket_id = 'avatars' );
+
+
+--
+-- Name: chat_user; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.chat_user (
+    created_at timestamp with time zone DEFAULT now(),
+    chat_id uuid NOT NULL,
+    user_id uuid NOT NULL
 );
 
-alter table profiles enable row level security;
 
-create policy "Public profiles are viewable by the owner."
-  on profiles for select
-  using ( auth.uid() = id );
+ALTER TABLE public.chat_user OWNER TO postgres;
 
-create policy "Users can insert their own profile."
-  on profiles for insert
-  with check ( auth.uid() = id );
+--
+-- Name: chats; Type: TABLE; Schema: public; Owner: postgres
+--
 
-create policy "Users can update own profile."
-  on profiles for update
-  using ( auth.uid() = id );
+CREATE TABLE public.chats (
+    id uuid DEFAULT extensions.uuid_generate_v4() NOT NULL,
+    created_at timestamp with time zone DEFAULT now()
+);
 
--- Set up Realtime
-begin;
-  drop publication if exists supabase_realtime;
-  create publication supabase_realtime;
-commit;
-alter publication supabase_realtime add table profiles;
 
--- Set up Storage
-insert into storage.buckets (id, name)
-values ('avatars', 'avatars');
+ALTER TABLE public.chats OWNER TO postgres;
 
-create policy "Avatar images are publicly accessible."
-  on storage.objects for select
-  using ( bucket_id = 'avatars' );
+--
+-- Name: messages; Type: TABLE; Schema: public; Owner: postgres
+--
 
-create policy "Anyone can upload an avatar."
-  on storage.objects for insert
-  with check ( bucket_id = 'avatars' );
+CREATE TABLE public.messages (
+    id uuid DEFAULT extensions.uuid_generate_v4() NOT NULL,
+    created_at timestamp with time zone DEFAULT now(),
+    chat_id uuid,
+    user_id uuid,
+    content text
+);
 
-create policy "Anyone can update an avatar."
-  on storage.objects for update
-  with check ( bucket_id = 'avatars' );
+
+ALTER TABLE public.messages OWNER TO postgres;
+
+--
+-- Name: profiles; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.profiles (
+    id uuid NOT NULL,
+    created_at timestamp with time zone DEFAULT now(),
+    username text,
+    full_name text,
+    avatar_url text,
+    CONSTRAINT username_length CHECK ((char_length(username) >= 3))
+);
+
+
+ALTER TABLE public.profiles OWNER TO postgres;
+
+--
+-- Name: chat_user chat_user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.chat_user
+    ADD CONSTRAINT chat_user_pkey PRIMARY KEY (chat_id, user_id);
+
+
+--
+-- Name: chats chats_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.chats
+    ADD CONSTRAINT chats_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: messages messages_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: profiles profiles_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.profiles
+    ADD CONSTRAINT profiles_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: profiles profiles_username_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.profiles
+    ADD CONSTRAINT profiles_username_key UNIQUE (username);
+
+
+--
+-- Name: chat_user chat_user_chat_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.chat_user
+    ADD CONSTRAINT chat_user_chat_id_fkey FOREIGN KEY (chat_id) REFERENCES public.chats(id) ON DELETE CASCADE;
+
+
+--
+-- Name: chat_user chat_user_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.chat_user
+    ADD CONSTRAINT chat_user_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.profiles(id) ON DELETE CASCADE;
+
+
+--
+-- Name: messages messages_chat_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_chat_id_fkey FOREIGN KEY (chat_id) REFERENCES public.chats(id) ON DELETE CASCADE;
+
+
+--
+-- Name: messages messages_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.messages
+    ADD CONSTRAINT messages_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.profiles(id) ON DELETE CASCADE;
+
+
+--
+-- Name: profiles profiles_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.profiles
+    ADD CONSTRAINT profiles_id_fkey FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE;
+
+
+--
+-- Name: chat_user ALL for authenticated; Type: POLICY; Schema: public; Owner: postgres
+--
+
+CREATE POLICY "ALL for authenticated" ON public.chat_user TO authenticated USING (true) WITH CHECK (true);
+
+
+--
+-- Name: chats ALL for authenticated; Type: POLICY; Schema: public; Owner: postgres
+--
+
+CREATE POLICY "ALL for authenticated" ON public.chats TO authenticated USING (true) WITH CHECK (true);
+
+
+--
+-- Name: messages ALL for authenticated; Type: POLICY; Schema: public; Owner: postgres
+--
+
+CREATE POLICY "ALL for authenticated" ON public.messages TO authenticated USING (true) WITH CHECK (true);
+
+
+--
+-- Name: profiles Public profiles are viewable by everyone.; Type: POLICY; Schema: public; Owner: postgres
+--
+
+CREATE POLICY "Public profiles are viewable by everyone." ON public.profiles FOR SELECT USING (true);
+
+
+--
+-- Name: profiles Users can insert their own profile.; Type: POLICY; Schema: public; Owner: postgres
+--
+
+CREATE POLICY "Users can insert their own profile." ON public.profiles FOR INSERT WITH CHECK ((auth.uid() = id));
+
+
+--
+-- Name: profiles Users can update own profile.; Type: POLICY; Schema: public; Owner: postgres
+--
+
+CREATE POLICY "Users can update own profile." ON public.profiles FOR UPDATE USING ((auth.uid() = id));
+
+
+--
+-- Name: chat_user; Type: ROW SECURITY; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.chat_user ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: chats; Type: ROW SECURITY; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.chats ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: messages; Type: ROW SECURITY; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.messages ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: profiles; Type: ROW SECURITY; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.profiles ENABLE ROW LEVEL SECURITY;
+
+
+
+--
+-- Name: supabase_realtime chat_user; Type: PUBLICATION TABLE; Schema: public; Owner: postgres
+--
+
+ALTER PUBLICATION supabase_realtime ADD TABLE ONLY public.chat_user;
+
+
+--
+-- Name: supabase_realtime chats; Type: PUBLICATION TABLE; Schema: public; Owner: postgres
+--
+
+ALTER PUBLICATION supabase_realtime ADD TABLE ONLY public.chats;
+
+
+--
+-- Name: supabase_realtime messages; Type: PUBLICATION TABLE; Schema: public; Owner: postgres
+--
+
+ALTER PUBLICATION supabase_realtime ADD TABLE ONLY public.messages;
+
+
+--
+-- Name: TABLE chat_user; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT ALL ON TABLE public.chat_user TO anon;
+GRANT ALL ON TABLE public.chat_user TO authenticated;
+GRANT ALL ON TABLE public.chat_user TO service_role;
+
+
+--
+-- Name: TABLE chats; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT ALL ON TABLE public.chats TO anon;
+GRANT ALL ON TABLE public.chats TO authenticated;
+GRANT ALL ON TABLE public.chats TO service_role;
+
+
+--
+-- Name: TABLE messages; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT ALL ON TABLE public.messages TO anon;
+GRANT ALL ON TABLE public.messages TO authenticated;
+GRANT ALL ON TABLE public.messages TO service_role;
+
+
+--
+-- Name: TABLE profiles; Type: ACL; Schema: public; Owner: postgres
+--
+
+GRANT ALL ON TABLE public.profiles TO anon;
+GRANT ALL ON TABLE public.profiles TO authenticated;
+GRANT ALL ON TABLE public.profiles TO service_role;
+
