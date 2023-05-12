429f43e99b58c53c076c7febf4cbb5784dc18360
Raúl Aguilar <ag21raul@gmail.com>
2023-05-12 13:28:08 +0200

    feat: friendly welcome message on chats

--- apps/web/pages/chats/index.vue
+++ apps/web/pages/chats/index.vue
@@ -15,7 +15,10 @@
             <ChatsList />
         </template>
         <template #messages>
-            <div>Messages</div>
+            <section class="welcome">
+                <h1>Welcome to NuChat</h1>
+                <h2>Just a chat app built with Nuxt 3 and Supabase</h2>
+            </section>
         </template>
     </NuxtLayout>
 </template>
@@ -29,5 +32,30 @@
         width: 70%;
         background-color: $secondary;
         margin: 0 auto;
+
+        .welcome {
+            display: flex;
+            flex-flow: column;
+            justify-content: center;
+            align-items: center;
+            height: 100%;
+            width: 70%;
+
+            h1 {
+                font-size: 3rem;
+                font-weight: 700;
+                color: $primary;
+                text-align: center;
+                margin-bottom: 1rem;
+            }
+
+            h2 {
+                font-size: 1.5rem;
+                font-weight: 500;
+                color: #ccc;
+                text-align: center;
+                width: 50%;
+            }
+        }
     }
 </style>
